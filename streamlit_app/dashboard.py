648e4ca92f247ca4f3c2f0ed022ae4e5b672d04e
Pankaj <pankajsarkate143@rediffmail.com>
2025-11-06 03:59:04 +0530

    https://telecom-churn-mlops-production.up.railway.app/predict

--- streamlit_app/dashboard.py
+++ streamlit_app/dashboard.py
@@ -30,7 +30,8 @@ payload = {
 #     except requests.exceptions.ConnectionError:
 #         st.error("❌ FastAPI server is not running at localhost:8000")
 
-API_URL = "telecom-churn-mlops-production.up.railway.app/predict"  # placeholder for future
+API_URL = "https://telecom-churn-mlops-production.up.railway.app/predict"  # placeholder for future
+
 
 if st.button("Predict", key="predict_button"):
     try:
